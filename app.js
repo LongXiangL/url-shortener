4d362d324a9bb1240c277a2dba433bf641cebc0e
LongXiangL <bfh1104@gmail.com>
2023-03-10 22:49:55 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 
-mongoose.connect('mongodb://localhost/url-shortener', {
+
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB 連接錯誤：'));
-db.once('open', function () {
-  console.log('Connected to MongoDB');
-});
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
+app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 
---
+++ models/seeds/url-shortenerSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Url = require('../url-shortener') // 載入 todo model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  Url.create({ originalUrl: `https://www.google.com/` })
+  console.log('done')
+})
