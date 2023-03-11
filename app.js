ff9a0117a8c1556642b3a9214a1a738f14415655
LongXiangL <bfh1104@gmail.com>
2023-03-11 22:28:02 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,80 +1,17 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
-const ShortUrl = require('./models/url-shortener') // 載入 model
+const routes = require('./routes')// 引用路由器
 const PORT = process.env.PORT || 3000
 require('./config/mongoose')
 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(routes)
 app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
 app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended: true }));
 
 
-// 設定首頁路由
 
-app.get('/', (req, res) => {
-  res.render('index', { indexActive: true });
-});
-
-// 設定縮址路由
-app.post("/", (req, res) => {
-  if (!req.body.url) return res.redirect("/")
-  const shortURL = generateShortUrl()
-
-  ShortUrl.findOne({ originalURL: req.body.url })
-    .then(data =>
-      data ? data : ShortUrl.create({ shortURL, originalURL: req.body.url })
-    )
-    .then(data =>
-      res.render("index", {
-        origin: req.headers.origin,
-        shortURL: data.shortURL,
-      })
-    )
-    .catch(error => console.error(error))
-})
-
-// 設定短網址轉址路由
-
-app.get("/:shortURL", (req, res) => {
-  const { shortURL } = req.params
-
-  ShortUrl.findOne({ shortURL })
-    .then(data => {
-      if (!data) {
-        return res.render("error", {
-          errorMsg: "Can't found the URL",
-          errorURL: req.headers.host + "/" + shortURL,
-        })
-      }
-
-      res.redirect(data.originalURL)
-    })
-    .catch(error => {
-      console.error(error)
-      res.render("error", {
-        errorMsg: "Error retrieving URL",
-        errorURL: req.headers.host + "/" + shortURL,
-      })
-    })
-})
-
-
-// 產生短網址的函式
-function generateShortUrl() {
-  const characters =
-    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const shortUrlLength = 5;
-  let shortUrl = '';
-
-  for (let i = 0; i < shortUrlLength; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    shortUrl += characters[randomIndex];
-  }
-
-  return shortUrl;
-}
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
---
+++ routes/modules/home.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const router = express.Router()
+const ShortUrl = require('../../models/url-shortener') // 引用 url model
+
+// 產生短網址的函式
+function generateShortUrl() {
+  const characters =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const shortUrlLength = 5;
+  let shortUrl = '';
+
+  for (let i = 0; i < shortUrlLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    shortUrl += characters[randomIndex];
+  }
+
+  return shortUrl;
+}
+
+
+
+// 設定首頁路由
+
+router.get('/', (req, res) => {
+  res.render('index', { indexActive: true });
+});
+
+
+// 設定縮址路由
+router.post('/', (req, res) => {
+  console.log(req.body); // 添加这一行来检查req.body对象的值
+
+  
+  if (!req.body.url) return res.redirect("/")
+  const shortURL = generateShortUrl()
+
+  ShortUrl.findOne({ originalURL: req.body.url })
+    .then(data =>
+      data ? data : ShortUrl.create({ shortURL, originalURL: req.body.url })
+    )
+    .then(data =>
+      res.render("index", {
+        origin: req.headers.origin,
+        shortURL: data.shortURL,
+      })
+    )
+    .catch(error => console.error(error))
+})
+
+// 設定短網址轉址路由
+
+router.get('/:shortURL', (req, res) => {
+  const { shortURL } = req.params
+
+  ShortUrl.findOne({ shortURL })
+    .then(data => {
+      if (!data) {
+        return res.render("error", {
+          errorMsg: "Can't found the URL",
+          errorURL: req.headers.host + "/" + shortURL,
+        })
+      }
+
+      res.redirect(data.originalURL)
+    })
+    .catch(error => {
+      console.error(error)
+      res.render("error", {
+        errorMsg: "Error retrieving URL",
+        errorURL: req.headers.host + "/" + shortURL,
+      })
+    })
+})
+
+
+
+
+// 匯出路由模組
+module.exports = router
